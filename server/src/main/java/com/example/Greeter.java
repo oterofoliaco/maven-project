89b2dcfb5a423e836800590d99ae57eb5422ebfc
Otero Foliaco <oterofoliaco@gmail.com>
2020-05-24 17:24:18 -0500

    commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,10 +1,22 @@
 package com.example;
 
+/**
+ * This is a class.
+ */
 public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
   public Greeter() {
 
   }
-  public final String greet(final String someone) {
+
+   /**
+   * @param someone
+   * @return hello mas entrada
+   */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
