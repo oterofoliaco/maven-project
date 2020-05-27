91911239c722614e980345f94e44752ebabc5509
Otero Foliaco <oterofoliaco@gmail.com>
2020-05-26 20:41:19 -0500

    commit ejemplo de pdate constructor

---
+++ webapp/src/main/webapp/ff.py
@@ -0,0 +1,11 @@
+inicio=1
+fin=10
+conteo=1
+conteo2=1
+while conteo<=fin:
+    conteo2=1
+    while conteo2<=fin:
+        print(conteo2*conteo,end='\t')
+        conteo2=conteo2+1
+    print('\n')
+    conteo=conteo+1
